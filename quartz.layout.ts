9225c71a9bb729d0a4770ddde127efbfa213ff6d
Kai Hagen <hagen@versino.de>
2024-07-08 16:17:05 +0200

    Quartz sync: Jul 8, 2024, 4:17 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.MobileOnly(Component.Explorer()),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
