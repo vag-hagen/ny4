7c8e3402caf6b2b8314d444295b8bfab846a2141
Kai Hagen <hagen@versino.de>
2024-07-08 14:24:40 +0200

    Quartz sync: Jul 8, 2024, 2:24 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,7 +13,7 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <hr />
+        <!--<hr />
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
@@ -24,7 +24,7 @@ export default ((opts?: Options) => {
               <a href={link}>{text}</a>
             </li>
           ))}
-        </ul>
+        </ul> -->
       </footer>
     )
   }
